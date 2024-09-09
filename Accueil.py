7b70c3ce2efb34019360ba03c9e669b853780132
Sodium Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2024-09-10 00:27:16 +0200

    Added decorations to the site

--- Accueil.py
+++ Accueil.py
@@ -7,13 +7,13 @@ notes,prosit = st.columns(2)
 
 with notes:
     st.header('Lecteur de notes')
-    #st.image()
+    st.image('res/notes.png',width=100)
     st.write("Le lecteur de notes permet facilement d'étudier son dossier de synthèse ainsi que de savoir quelles "
              "épreuves doivent être rattrapées.")
-    st.page_link('pages/Lecteur_de_notes.py',label='Y aller')
+    st.page_link('pages/Lecteur_de_notes.py',label='Y aller',icon=':material/arrow_forward:')
 
 with prosit:
     st.header('Éditeur de prosit')
-    #st.image()
+    st.image('res/editeur.png',width=100)
     st.write("L'éditeur de prosit permet de créer de A à Z un PDF prêt à être envoyé du prosit aller en cours.")
-    st.page_link('pages/Éditeur_de_prosit.py',label='Y aller')
+    st.page_link('pages/Éditeur_de_prosit.py',label='Y aller',icon=':material/arrow_forward:')
