5b88d38f610322a3383be529516fb41d7e31b83c
Sodium Bismuth <90388021+SodiumBismuth@users.noreply.github.com>
2024-09-19 11:30:50 +0200

    Prosit Editor

--- pages/Éditeur_de_prosit.py
+++ pages/Éditeur_de_prosit.py
@@ -1,4 +1,90 @@
 import streamlit as st
+from streamlit_tags import st_tags
+from markdown_pdf import MarkdownPdf
+from markdown_pdf import Section
 
 st.title('Éditeur de prosit')
-st.warning("Cette partie est en cours de développement, revenez plus tard ou utilisez https://prosit.hugochampy.fr/")
+
+titre = st.text_input("Titre du prosit")
+
+prosit_steps = [
+    "Mots Clés",
+    "Contexte",
+    "Problématique",
+    "Contraintes",
+    "Généralisation",
+    "Livrable",
+    "Pistes de solution",
+    "Plan D'action"
+]
+
+kw, ctx, pb, ct, gn, lv, ps, pa = st.tabs(prosit_steps)
+
+# mots clés
+
+with kw:
+    keywords = st_tags(label="Entrez vos mots clés ici", text="Appuyez sur entrée pour ajouter un nouveau mot")
+
+with ctx:
+    context = st.text_area("Entrez votre/vos contexte(s) ici", height=475)
+
+with pb:
+    problematic = st.text_input("Entrez la problématique")
+
+with ct:
+    constraints = st.text_area("Insérez vos contraintes ici", height=475)
+
+with gn:
+    generalization = st_tags(label="Entrez vos mots de généralisation ici",
+                             text="Appuyez sur entrée pour ajouter un nouveau mot")
+
+with lv:
+    deliverable = st.text_area("Insérez ce que le livrable devra contenir ici", height=475)
+
+with ps:
+    solution = st.text_area("Insérez les pistes de solutions ici", height=475)
+
+with pa:
+    action_plan = st.text_area("Insérez votre plan d'action ici", height=475)
+
+
+# Fonction pour générer le PDF
+def generate_pdf():
+    # mise en forme des listes
+    f_keywords = " - " + "\n - ".join(keywords)
+    f_generalization = " - " + "\n - ".join(generalization)
+    # Créer un contenu en Markdown avec les entrées utilisateur
+    with open("template.txt", 'r',encoding="utf-8") as f:
+        md_content = f.read().format(
+            titre,
+            f_keywords,
+            context,
+            problematic,
+            constraints,
+            f_generalization,
+            deliverable,
+            solution,
+            action_plan
+        )
+
+    # Convertir le markdown en PDF
+
+    pdf = MarkdownPdf(toc_level=1)
+    pdf.add_section(Section(md_content), user_css="body {font-family:\"Arial\"}")
+    pdf.writer.close()
+    return pdf.out_file
+
+
+if st.button("Generate PDF"):
+    # Générer le PDF
+    pdf_buffer = generate_pdf()
+
+    print(pdf_buffer.read())
+
+    # Convertir le buffer en fichier téléchargeable
+    st.download_button(
+        label="Download PDF",
+        data=pdf_buffer,
+        file_name=f"{titre}.pdf",
+        mime="application/pdf"
+    )
